ffc652a8ec583f09f6bcaaa5e6a823b3fb3268c5
Muhammed Midlaj <midlaj4n@gmail.com>
2022-12-05 18:27:07 +0530

    ToDo First Model

--- ToDo_App/ToDoAPP/urls.py
+++ ToDo_App/ToDoAPP/urls.py
@@ -8,7 +8,11 @@ app_name = 'ToDoAPP'
 urlpatterns = [
     path('', views.task, name='task'),
     path('delete/<int:id>/', views.delete, name='delete'),
-    path('update/<int:id>/', views.update, name='update')
+    path('update/<int:id>/', views.update, name='update'),
+    path('classhome/', views.TaskListView.as_view(), name='classhome'),
+    path('classdetailview/<int:pk>/', views.TaskDetailView.as_view(), name='classdetailview'),
+    path('updateview/<int:pk>/', views.TaskUpdateView.as_view(), name='updateview'),
+    path('deleteview/<int:pk>/', views.TaskDeleteView.as_view(), name='deleteview')
 
 ]
 
--- ToDo_App/ToDoAPP/views.py
+++ ToDo_App/ToDoAPP/views.py
@@ -1,8 +1,42 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
 from .forms import TaskForm
 from .models import Task
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+
+
+# Using the generic class based view
+class TaskListView(ListView):
+    model = Task
+    template_name = 'list.html'
+    context_object_name = 'task_key'
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'detail.html'
+    context_object_name = 'task_id'
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'update_view.html'
+    context_object_name = 'task_id'
+    fields = ('task', 'priority', 'date')
+
+    def get_success_url(self):
+        return reverse_lazy('ToDoAPP:classdetailview', kwargs={'pk': self.object.id})
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'delete.html'
+    context_object_name = 'task'
+    success_url = reverse_lazy('ToDoAPP:classhome')
 
 
 # Create your views here.
@@ -15,7 +49,7 @@ def task(request):
         task_s = Task(task=task1, priority=priority, date=date)
         task_s.save()
         return redirect('/')
-    return render(request, 'home.html',{'task_key': task})
+    return render(request, 'home.html', {'task_key': task})
 
 
 def delete(request, id):
